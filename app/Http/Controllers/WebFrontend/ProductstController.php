cc03013a075a2db2ce1b859deadb36278df2614a
cc <nyin1765@gmail.com>
2021-11-06 15:18:41 +0630

    route with controller

---
+++ app/Http/Controllers/WebFrontend/ProductstController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\WebFrontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductstController extends Controller
+{
+    public function productsPage($id, $shop_id)
+    {
+        return "This is Products Page for  product_id " . $id . " For Shop_id " . $shop_id;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,27 @@
 <?php
 
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\WebFrontend\AboutController;
+use App\Http\Controllers\WebFrontend\ProductstController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', [AboutController::class, 'aboutPage']);
+
+// Controller Use
+Route::get('/contact', [Controller::class, 'contactPage']);
+
+
+Route::get('/products', function () {
+    return view('products');
+});
+
+Route::get('/shop', function () {
+    return view('shops.shop');
+});
+
+// Route Parameta two
+Route::get('/products/{id}/shop/{shop_id}', [ProductstController::class, 'productsPage']);
